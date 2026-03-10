d8d0ceeb5098d24570a5ea8d663a2ef853bf277e
Sanaulla Haq <sanaullahaq01@gmail.com>
2026-03-10 14:53:15 +0600

    lc

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -1,9 +1,41 @@
+import 'dart:collection';
 import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'arguments.dart';
 
 class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+
   /// Runs the command-line application logic with the given arguments.
   Future<void> run(List<String> input) async {
-    print('CommandRunner received argument $input');
+    final ArgResults results = parse(input);
+    if(results.command != null) {
+      Object? output = await results.command!.run(results);
+      print(output.toString());
+    }
+  }
+
+  void addCommand(Command command){
+    // TODO: handle error (Commands can't have names that conflict)
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  // Returns usage for executable only.
+  // Should be overridden if you aren't using [HelpCommand]
+  // or another means of printing usage
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
   }
 }
