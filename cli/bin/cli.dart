d7131db1c75bb038b196baa513bbb1af5eb97730
Sanaulla Haq <sanaullahaq01@gmail.com>
2026-05-07 17:53:41 +0600

    DartPedia Last Chapter

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,25 +1,33 @@
-// import 'package:cli/cli.dart' as cli;
+import 'package:cli/cli.dart';
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
   /**
    * .. lets you call multiple methods/set multiple properties on the same object, without repeating the variable name. It always returns the original object, not the result of the method call.
    */
-  var commandRunner = CommandRunner(
-    onOutput: (String output) async {
-      await write(output);
-    },
-    onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
-      if (error is Exception) {
-        print(error);
-      }
-    },
-  )..addCommand(HelpCommand());   // .. Cascade Operator skips the below line.
-  // commandRunner.addCommand(HelpCommand());
-  commandRunner.run(arguments);
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  app.run(arguments);
 }
